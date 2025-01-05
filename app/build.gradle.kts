49afbe8199a6e30db09edceae63be67509e64daf
Lobanokivan11 <lobanokivan11@gmail.com>
2025-01-05 14:45:25 +0300

    %

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         // to the substratum repo rather than anything lower. Keep targetSdkVersion the same.
         minSdk = 24
         // Both versions must be changed to increment on Play Store/user's devices
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         // Themers: DO NOT MODIFY
         buildConfigField("boolean", "SUPPORTS_THIRD_PARTY_SYSTEMS", "$SUPPORTS_THIRD_PARTY_SYSTEMS")
